e4b6b1b24d13b0a6653a55bda1a9a3a37ebdd239
Gayan Witharana <gayan@thinkcube.com>
2018-08-06 13:10:29 +0530

    added e2e testing

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default class App extends React.Component {
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text>Open up App.js to start working on your app!</Text>
+  //       <Text>Changes you make will automatically reload.</Text>
+  //       <Text>Shake your phone to open the developer menu.</Text>
+  //     </View>
+  //   );
+  // }
+
+  state = {
+    counter: 0,
+    text: ""
+  };
+
+  onPress = () => this.setState({ counter: this.state.counter + 1 });
+  onChange = text => this.setState({ text });
+
   render() {
+    const { counter, text } = this.state;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.container} accessibilityLabel="testview">
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+
+        <Text accessibilityLabel="counter">{counter}</Text>
+        <Button
+          onPress={this.onPress}
+          title="Press me"
+          accessibilityLabel="button"
+        />
+        <TextInput accessibilityLabel="avengers" value={text} onChangeText={this.onChange} />
       </View>
     );
   }
@@ -16,8 +41,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ __tests__/appium.test.js
@@ -0,0 +1,41 @@
+import wd from "wd";
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+const PORT = 4723;
+const config = {
+  platformName: "Android",
+  deviceName: "Android Emulator",
+  app: "./android/app/build/outputs/apk/app-debug.apk" // relative to root of project
+};
+const driver = wd.promiseChainRemote("localhost", PORT);
+
+beforeAll(async () => {
+  await driver.init(config);
+  await driver.sleep(2000); // wait for app to load
+});
+
+test("appium renders", async () => {
+  expect(await driver.hasElementByAccessibilityId("testview")).toBe(true);
+  expect(await driver.hasElementByAccessibilityId("notthere")).toBe(false);
+});
+
+test("appium button click", async () => {
+  expect(await driver.hasElementByAccessibilityId("button")).toBe(true);
+  await driver
+    .elementByAccessibilityId("button")
+    .click()
+    .click()
+    .click()
+    .click()
+    .click();
+
+  const counter = await driver.elementByAccessibilityId("counter").text();
+  expect(counter).toBe("5");
+});
+
+test("avenger text input", async() => {
+  const TEXT = "avengers infinity war";
+  await driver.elementByAccessibilityId("avengers").type(TEXT);
+  const resultText = await driver.elementByAccessibilityId("avengers").text();
+  expect(resultText).toBe(TEXT);
+});
